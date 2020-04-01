63597c01ddf0a60d13fe667d5de45f2eda1c79f6
Tomas iMac <tomas.baran@gmail.com>
2020-04-02 00:30:55 +0100

    Update screenshot.png

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         //canvasColor: Colors.transparent,
         primaryColor: Colors.lightBlueAccent,
